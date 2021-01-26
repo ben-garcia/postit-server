a90ed5727841aa19bcde8dd2b3d7244e79805859
= <7rubengarcia7@gmail.com>
2021-01-26 11:48:38 -0800

    Added eslint and prettier

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import express from 'express';
 const app = express();
 
 app.get('/', (_, res) => {
-	res.json({hello: 'world' });
-})
+  res.json({ hello: 'world' });
+});
 
 app.listen(8888, () => console.log('listening on port 8888'));
